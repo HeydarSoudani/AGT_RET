ca5507e669a1ab5c355bb6077f6312c6b89a2efb
Heydar Soudani <heydar.soudani@gmail.com>
2025-12-07 19:48:27 +0100

    some files added

---
+++ c1_baseline_retrievers/src/agentic_retrieval_models.py
@@ -0,0 +1,12 @@
+
+
+
+# TODO: ReAct as a retriever model
+# TODO: LLM4CS & MQ4CS: 
+#   - Generating Multi-Aspect Queries for Conversational Search: https://arxiv.org/pdf/2403.19302
+#   - LLM4CS: Large language models know your contextual search intent: A prompting framework for conversational search.: https://aclanthology.org/2023.findings-emnlp.86/
+
+# TODO: RMIT–ADM+S at the SIGIR 2025 LiveRAG Challenge
+#  - Paper (report): https://arxiv.org/pdf/2506.14516
+#  - Code: https://github.com/rmit-ir/G-RAG-LiveRAG
+
